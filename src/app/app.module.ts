1b761976da64b5e0f54d5c8257ca040b6190dedd
Cristian Arevalo <cristianarevaloduran@gmail.com>
2022-05-20 11:50:07 -0500

    cristian

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MascotasComponent } from './mascotas/mascotas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MascotasComponent,
   ],
   imports: [
     BrowserModule
